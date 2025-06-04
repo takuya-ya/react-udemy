d7e7b72769a56122c5c387cf5845f94488d84bf8
takuya <cdwbh564@yahoo.co.jp>
2025-06-04 19:30:31 +0900

    ~ イベントに合わせて関数を実行してみよう

--- 04_react_basic/src/110_props_children/start/Example.jsx
+++ 04_react_basic/src/110_props_children/start/Example.jsx
@@ -3,13 +3,22 @@ import Container from "./components/Container";
 
 const profile = [
   { name: "Takashi", age: 19, country: "Japan", color: "green" },
-  { name: "Jane", age: 28, country: "UK", color: "blue" },
+  { name: "Jane", age: 28, country: "UK", color: "red" },
 ];
 
 const Example = () => {
   return (
     <div>
-      <Container title="Childrenとは？" />
+      <Container title='まとめて渡す'>
+        <Profile {...profile[0]}/>
+        <Profile {...profile[1]}/>
+      </Container>
+
+      <Container>
+        <Profile {...profile[1]}/>
+        <Profile {...profile[0]}/>
+      </Container>
+
     </div>
   );
 };
