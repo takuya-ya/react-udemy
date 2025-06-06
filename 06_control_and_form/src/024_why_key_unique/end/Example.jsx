47ef3d8a1627abf43a490407a233c8d577be4025
Your Name <your-email@example.com>
2025-06-06 12:44:03 +0900

    【練習】ステートの受け渡し

--- 06_control_and_form/src/024_why_key_unique/end/Example.jsx
+++ 06_control_and_form/src/024_why_key_unique/end/Example.jsx
@@ -2,16 +2,20 @@ import "./Example.css";
 import { useState } from "react";
 
 const Example = () => {
+  // 表示する内容作成
   const inputFact = () => ({
     key: Math.floor(Math.random() * 1e3),
     value: <input />,
   });
 
+  // stateにセット
   const [inputs, setInputs] = useState([inputFact(), inputFact(), inputFact()]);
 
+  // 先頭に追加するボタンに対応したstateの実行
   const unshiftInput = () => {
     setInputs((prev) => [inputFact(), ...prev]);
   };
+
   return (
     <>
       <button onClick={unshiftInput}>先頭に追加</button>
@@ -28,6 +32,7 @@ const Example = () => {
         </div>
         <div>
           <strong>{`key={index}`}</strong>
+          {/* 第2引数：その要素のインデックス番号が自動で入る */}
           <ul>
             {inputs.map((input, index) => (
               <li key={index}>
