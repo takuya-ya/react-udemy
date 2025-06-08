627b185094bc6d127cae71e68897fad7407d00b2
takuya <cdwbh564@yahoo.co.jp>
2025-06-08 20:57:15 +0900

    【Form】プルダウンの作成方法 : optionタグで表示

--- 06_control_and_form/src/120_select/start/Example.jsx
+++ 06_control_and_form/src/120_select/start/Example.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 const Example = () => {
+  const [selected, setSelected] = useState('Banana');
   return (
+    <>
     <p style={{ textAlign: "center" }}>
       startフォルダの内容が表示されます。
       <br />
       練習用に使ってください！
     </p>
+
+    <select value={selected}  onChange={(e) => setSelected(e.target.value)}>
+        <option value="Apple">Apple</option>
+        <option value="Banana">Banana</option>
+        <option value="Cherry">Cherry</option>
+    </select>
+
+    <div>
+      選択された果物 : {selected}
+    </div>
+    </>
   );
 };
 
