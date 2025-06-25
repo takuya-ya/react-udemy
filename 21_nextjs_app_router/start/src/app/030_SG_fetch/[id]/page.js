fe2aad39921a678e9e3f917b0fc435fd6105285f
takuya <cdwbh564@yahoo.co.jp>
2025-06-25 23:15:47 +0900

    APIで取得したデータをSGに使ってみよう

--- 21_nextjs_app_router/start/src/app/030_SG_fetch/[id]/page.js
+++ 21_nextjs_app_router/start/src/app/030_SG_fetch/[id]/page.js
@@ -1,6 +1,13 @@
 import { ENDPOINT } from "@/constants";
 import Article from "../../../components/article";
 
+export async function generateStaticParams() {
+  const data = await fetch(ENDPOINT).then(res => res.json());
+  // console.log(data);
+  // () は オブジェクトリテラルであることを示す記号です（return の省略ではない）
+  return data.map(record => ({ id: record.id }));
+}
+
 export default async function Detail({ params }) {
   const article = await fetch(`${ENDPOINT}/${params.id}`).then((res) =>
     res.json()
