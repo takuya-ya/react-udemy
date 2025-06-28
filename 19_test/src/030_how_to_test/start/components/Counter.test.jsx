13560c3a9966eaeab9c59a6dac43dd29bafbd3bc
takuya <cdwbh564@yahoo.co.jp>
2025-06-28 23:54:51 +0900

    test : Counterコンポーネントのカウントアップ動作を検証するテストを作成
    - チャプター：テストコードの書き方の基礎を学ぼう

--- 19_test/src/030_how_to_test/start/components/Counter.test.jsx
+++ 19_test/src/030_how_to_test/start/components/Counter.test.jsx
@@ -3,4 +3,21 @@ Arrage:(テストデータ、条件、環境の設定).
 Act:(ロジックの実行、関数の実行).
 Assertion:(実行結果と期待する結果の比較).
 */
-test('ダミーテスト:削除して記載してください。', () => {});
+
+import { fireEvent, render, screen } from "@testing-library/react";
+import Counter from "./Counter";
+
+test('カウントアップ押下で現在のカウントが+1されるか', () => {
+  render(<Counter originCount={0} />);
+  const btn = screen.getByRole('button', { name:'カウントアップ'});
+
+  const spanElBeforeUpdate = screen.getByText('現在のカウント:0')
+  expect(spanElBeforeUpdate).toBeInTheDocument();
+
+  fireEvent.click(btn);
+
+  const spanEl = screen.getByText('現在のカウント:1')
+  expect(spanEl).toBeInTheDocument();
+
+
+});
