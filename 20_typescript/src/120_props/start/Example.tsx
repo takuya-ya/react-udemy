fab2d3c9b5208cd6e5750aac90a208d4a744dad2
takuya <cdwbh564@yahoo.co.jp>
2025-07-02 01:23:48 +0900

    feat: btnコンポーネントとFnProps型を追加
    - FnProps型を定義
    - Btnコンポーネントを作成しonClickでprops.fnを呼ぶ

--- 20_typescript/src/120_props/start/Example.tsx
+++ 20_typescript/src/120_props/start/Example.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import Hello from './Hello.tsx';
+import Hello, {Btn} from './Hello.tsx';
 
 const Example: React.FC = () => {
   // 練習用
-  return <Hello text='TypeScript'>Children</Hello>
+  return (
+    <>
+      <Btn fn={(text) => console.log(`Hello ${text}`)}/>
+      <Hello text='TypeScript'>Children</Hello>
+    </>
+  );
 };
 
 export default Example;
--- 20_typescript/src/120_props/start/Hello.tsx
+++ 20_typescript/src/120_props/start/Hello.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 type HelloProps = {
   text: string
   children: React.ReactNode
@@ -6,4 +8,11 @@ type HelloProps = {
 const Hello: React.FC<HelloProps> = (props) => {
   return <h1>Hello {props.text}! {props.children}</h1>;
 }
+
+type FnProps = {
+  fn: (text: string) => void
+}
+
+export const Btn: React.FC<FnProps> = (props) => {return <button onClick={() => props.fn('TypeScript')}>ボタン</button>}
+
 export default Hello;
