e7b77de311a8e95741fbc787232c32a3a1396fde
takuya <cdwbh564@yahoo.co.jp>
2025-08-22 23:49:36 +0900

    4-37. 【練習＆解答】propsで値を渡してみよう

--- 04_react_basic/src/100_practice_props/start/components/Profile.jsx
+++ 04_react_basic/src/100_practice_props/start/components/Profile.jsx
@@ -1,10 +1,12 @@
 import "./Profile.css";
 
-const Profile = (props) => {
+const Profile = ({name = "John Doe", age = "??", country = "Japan" }) => {
   return (
-    <div className="profile">
-
-    </div>
+  <div className="profile">
+    <h3>Name: {name}</h3>
+    <h3>Age: {age}</h3>
+    <h3>From: {country}</h3>
+  </div>
   );
 };
 
