486a9b5056731f617b7c6eb0f7d2ab1ec22858aa
takuya <cdwbh564@yahoo.co.jp>
2025-08-24 23:56:31 +0900

    51. 【練習】ステートの処理を自分で記述してみよう

--- 05_state_and_event/src/055_practice_state/start/Example.jsx
+++ 05_state_and_event/src/055_practice_state/start/Example.jsx
@@ -1,20 +1,18 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const Example = () => {
-  const [count, setCount] = useState(0);
+  const [counte, setCounte] = useState(0);
 
   const countUp = () => {
-    // 更新用関数に渡す引数がstateの
-    setCount(state => state + 1);
+    setCounte((state) => state + 1);
   };
-  
   const countDown = () => {
-    setCount(state => state - 1);
+    setCounte((state) => state - 1);
   };
 
   return (
     <>
-      <p>現在のカウント数: {count}</p>
+      <p>現在のカウント数: {counte}</p>
       <button onClick={countUp}>+</button>
       <button onClick={countDown}>-</button>
     </>
