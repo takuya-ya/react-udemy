041bc6c272c87e71779720deadfc71ee2686278c
takuya-ya <cdwbh564@yahoo.co.jp>
2025-10-05 22:53:43 +0900

    65. 【練習】フィルターメソッドの使い方を練習してみよう

--- 06_control_and_form/src/050_practice_filter/start/Example.jsx
+++ 06_control_and_form/src/050_practice_filter/start/Example.jsx
@@ -20,29 +20,23 @@ const persons = [
 ];
 
 const Example = () => {
-  const [filterVal, setFilterVal] = useState('');
-  // person渡されるはず、勝手に配列の値が入る？つまりfilterの引数は配列？配列なら配列しか入らない？でないと配列かどうか判別がひつようになるよね
-  // fileter内のコールバック関数の引数は選択できる　今回はpersons配列の個別の値であるpersonのみ
-  const isMatch = (person) => {
-    // ;ってつけるっけ？タグにつけないだけ？
-    return person.name.indexOf(filterVal) !== -1;
-  };
+  const [inputText, setInputText] = useState("");
 
   return (
     <>
-    {/* onChange=の後にjsをきにゅうする場合、JSX内なので｛｝が必要。文字列を渡すなら不要。 */}
-      <input type="text" value={filterVal} onChange={(e) => {setFilterVal(e.target.value)}}/>
+      <input
+        type="text"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+      />
       <ul>
         {persons
-        // filterは配列に対してしか使えない。
-        // ×　即実行している
-        // .filter( isMatch(person) )
-        .filter( isMatch )
-        .map((person) => (
-          <li key={person.name}>
-            <Profile {...person} />
-          </li>
-        ))}
+          .filter((person) => person.name.indexOf(inputText) !== -1)
+          .map((person) => (
+            <li key={person.name}>
+              <Profile {...person} />
+            </li>
+          ))}
       </ul>
     </>
   );
