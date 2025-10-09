65fb36a0b864f1a7a851f6af4d513cee3a5b2239
takuya-ya <cdwbh564@yahoo.co.jp>
2025-10-09 23:59:45 +0900

    71. 【Form】複数選択チェックボックスの作成方法

--- 06_control_and_form/src/110_multi_checkbox/start/Example.jsx
+++ 06_control_and_form/src/110_multi_checkbox/start/Example.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
-// POINT 複数チェックボックスの実装
+const prefix = "start-";
+
 const Example = () => {
   const [fruits, setFruits] = useState([
     { label: "Apple", value: 100, checked: false },
@@ -8,58 +9,43 @@ const Example = () => {
     { label: "Cherry", value: 300, checked: false },
   ]);
 
-  const [sum, setSum] = useState(0);
-
   const handleChange = (e) => {
     const newFruits = fruits.map((fruit) => {
       const newFruit = { ...fruit };
       if (newFruit.label === e.target.value) {
         newFruit.checked = !fruit.checked;
       }
-
       return newFruit;
     });
 
     setFruits(newFruits);
-    // forEachバージョン
-    // let sumVal = 0;
-    // newFruits.forEach(fruit => {
-    //   if(fruit.checked) {
-    //     sumVal = sumVal + fruit.value;
-    //   }
-    // });
 
-    // filter + forEachバージョン
-    // let sumVal = 0;
-    // newFruits
-    //   .filter((fruit) => fruit.checked)
-    //   .forEach((fruit) => (sumVal = sumVal + fruit.value));
-
-    // filter + reduceバージョン
     let sumVal = newFruits
       .filter((fruit) => fruit.checked)
       .reduce((sumVal, fruit) => sumVal + fruit.value, 0);
     setSum(sumVal);
   };
+
+  const [sum, setSum] = useState(0);
+
   return (
     <div>
       {fruits.map((fruit) => {
         return (
           <div key={fruit.label}>
             <input
-              id={fruit.label}
               type="checkbox"
               value={fruit.label}
               checked={fruit.checked}
               onChange={handleChange}
             />
-            <label htmlFor={fruit.label}>
+            <label htmlFor={prefix + fruit.label}>
               {fruit.label}:{fruit.value}
             </label>
           </div>
         );
       })}
-      <div>合計：{sum}</div>
+      <div>合計:{sum}</div>
     </div>
   );
 };
