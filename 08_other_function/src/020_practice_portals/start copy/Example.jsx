c64da365ef1cfe52b916789038dd2131b94ad7aa
takuya-ya <cdwbh564@yahoo.co.jp>
2025-10-25 12:18:44 +0900

    87.【createPortal】モーダルの作り方
    88.【Bubbling】Portalを使う際の注意点！
    
    89.【練習＆解答】createPortalでトーストを作成してみよう

--- 08_other_function/src/020_practice_portals/start copy/Example.jsx
+++ 08_other_function/src/020_practice_portals/start copy/Example.jsx
@@ -1,25 +1,18 @@
 import { useState } from "react";
-import { createPortal } from "react-dom";
-
 import Toast from "./components/Toast";
-
-const ToastPortal = ({ children }) => {
-  const target = document.querySelector(".container.start");
-  return createPortal(children, target);
-};
 
 const Example = () => {
   const [toastOpen, setToastOpen] = useState(false);
 
   return (
     <div>
-      <h3>トーストの作成（createPortal）</h3>
-      <p>
-        ボタンを押すと.container.start要素にマウントされて、表示されるトーストを作成してください。トーストにはToastコンポーネントを使用してください。
-      </p>
+      <h3>
+        トーストの作成（createPortal）
+      </h3>
+      <p>ボタンを押すと.container.start要素にマウントされて、表示されるトーストを作成してください。トーストにはToastコンポーネントを使用してください。</p>
 
       <div className="container start"></div>
-
+      
       <button
         type="button"
         onClick={() => setToastOpen(true)}
@@ -28,12 +21,10 @@ const Example = () => {
         トーストを表示する
       </button>
       {toastOpen && (
-        <ToastPortal>
           <Toast
             visible={toastOpen}
             handleCloseClick={() => setToastOpen(false)}
           />
-        </ToastPortal>
       )}
     </div>
   );
