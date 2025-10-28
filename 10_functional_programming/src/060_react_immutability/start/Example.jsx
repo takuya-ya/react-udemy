6a332701a455757ef8e62296f20b1b1d807e4485
takuya-ya <cdwbh564@yahoo.co.jp>
2025-10-28 12:01:24 +0900

    110. Reactにおける不変性

--- 10_functional_programming/src/060_react_immutability/start/Example.jsx
+++ 10_functional_programming/src/060_react_immutability/start/Example.jsx
@@ -1,20 +1,28 @@
 import { useState } from "react";
 
-const Child = (props) => {
+const Child = ({ state, setState }) => {
+  const increment = () => {
+    setState((perv) => {
+      const newState = { value: perv.value + 1 };
+      return newState;
+    });
+  };
+
   return (
     <>
-      <span>{props.state.value}</span>
+      <span>{state.value}</span>
+      <button onClick={increment}>+</button>
     </>
   );
 };
 
 const Example = () => {
-  const [ state, setState ] = useState({ value: 0 });
+  const [state, setState] = useState({ value: 0 });
 
   return (
     <>
       <div>
-        <Child state={state}/>
+        <Child state={state} setState={setState} />
       </div>
     </>
   );
