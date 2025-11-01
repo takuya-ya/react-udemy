e24ed297b4c0de1edb0aebb32d9f8f8215218c2e
takuya-ya <cdwbh564@yahoo.co.jp>
2025-11-01 23:39:50 +0900

    wip:120. useContextのリファクタリングをしてみよう

--- 11_hooks_p1/src/050_context_file/start/Example.jsx
+++ 11_hooks_p1/src/050_context_file/start/Example.jsx
@@ -2,16 +2,28 @@ import { useState } from "react";
 import "./Example.css";
 
 const Example = () => {
-  const [theme, setTheme] = useState('light')
-  
-  const changeTheme = (e) => setTheme(e.target.value)
-  
-  const THEMES = ['light', 'dark', 'red'];
+  const [theme, setTheme] = useState("light");
+
+  const changeTheme = (e) => setTheme(e.target.value);
+
+  const THEMES = ["light", "dark", "red"];
 
   return (
     <>
       <header className={`content-${theme}`}>
-        
+        {THEMES.map((_theme) => {
+          return (
+            <label key={_theme}>
+              <input
+                type="radio"
+                value={_theme}
+                checked={_theme === theme}
+                onChange={changeTheme}
+              />
+              {_theme}
+            </label>
+          );
+        })}
       </header>
       <main className={`content-${theme}`}>
         <h1>テーマの切り替え</h1>
