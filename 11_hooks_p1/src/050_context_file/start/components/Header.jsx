5e19df6d04de4402c7d9bbf0c3ba2a2edf848802
takuya-ya <cdwbh564@yahoo.co.jp>
2025-11-05 22:48:25 +0900

    120. useContextのリファクタリングをしてみよう

--- 11_hooks_p1/src/050_context_file/start/components/Header.jsx
+++ 11_hooks_p1/src/050_context_file/start/components/Header.jsx
@@ -1,5 +1,27 @@
+import { useTheme } from "../context/ThemeContext";
+
 const Header = () => {
-  
+  const [theme, setTheme] = useTheme();
+  const changeTheme = (e) => setTheme(e.target.value);
+  const THEMES = ["light", "dark", "red"];
+
+  return (
+    <header className={`content-${theme}`}>
+      {THEMES.map((_theme) => {
+        return (
+          <label key={_theme}>
+            <input
+              type="radio"
+              value={_theme}
+              checked={_theme === theme}
+              onChange={changeTheme}
+            />
+            {_theme}
+          </label>
+        );
+      })}
+    </header>
+  );
 };
 
 export default Header;
---
+++ 11_hooks_p1/src/050_context_file/start/context/ThemeContext.jsx
@@ -0,0 +1,17 @@
+import { createContext } from "react";
+import { useState } from "react";
+import { useContext } from "react";
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  return (
+    <ThemeContext.Provider value={[theme, setTheme]}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
